35fd6fba4a83d346ba58f6d6c0bd34aad77e806a
blackbrokkoli <kastomat@gmail.com>
2023-04-07 16:46:53 +0200

    replace UI framework

--- main/views/queue.py
+++ main/views/queue.py
@@ -3,10 +3,12 @@ from ..models import *
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+
 @login_required(login_url='/accounts/login/')
 def queue(request):
     # get a random card from user that is due
     profile = request.user.profile
-    new_card = Card.objects.filter(profile=profile, due_at__lt=timezone.now(), occurrences__gt=0).order_by('?').first()
+    new_card = Card.objects.filter(profile=profile, due_at__lt=timezone.now(), occurrences__gt=0, is_active=True
+                                   ).order_by('?').first()
 
     return render(request, 'pages/queue.html', {'card': new_card})
